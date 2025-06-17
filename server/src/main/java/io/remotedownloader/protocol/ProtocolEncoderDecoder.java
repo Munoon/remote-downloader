20069e6425830b2667e51481b800d3e5afbaa73c
Munoon <munoongg@gmail.com>
2025-06-17 11:30:23 +0300

    Refactor ProtocolEncoderDecoder

--- server/src/main/java/io/remotedownloader/protocol/ProtocolEncoderDecoder.java
+++ server/src/main/java/io/remotedownloader/protocol/ProtocolEncoderDecoder.java
@@ -1,16 +1,54 @@
 package io.remotedownloader.protocol;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageCodec;
+import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
-public class ProtocolEncoderDecoder extends MessageToMessageCodec<BinaryWebSocketFrame, StringMessage> {
+public class ProtocolEncoderDecoder extends ChannelDuplexHandler {
+    private static final Logger log = LogManager.getLogger(ProtocolEncoderDecoder.class);
+
     @Override
-    protected void encode(ChannelHandlerContext ctx, StringMessage msg, List<Object> out) {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof BinaryWebSocketFrame frame) {
+            try {
+                StringMessage stringMessage = decode(frame);
+                ctx.fireChannelRead(stringMessage);
+            } catch (Exception e) {
+                log.warn("Failed to decode the message", e);
+            } finally {
+                frame.release();
+            }
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    private StringMessage decode(BinaryWebSocketFrame msg) {
+        ByteBuf in = msg.content();
+
+        int id = in.readInt();
+        short command = in.readShort();
+        String data = in.isReadable() ? in.toString(StandardCharsets.UTF_8) : null;
+        return new StringMessage(id, command, data);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        if (msg instanceof StringMessage stringMessage) {
+            BinaryWebSocketFrame frame = encode(ctx, stringMessage);
+            ctx.write(frame, promise);
+        } else {
+            ctx.write(msg, promise);
+        }
+    }
+
+    private BinaryWebSocketFrame encode(ChannelHandlerContext ctx, StringMessage msg) {
         String data = msg.data();
         byte[] dataBytes = data != null && !data.isEmpty() ? data.getBytes(StandardCharsets.UTF_8) : new byte[0];
 
@@ -19,17 +57,7 @@ public class ProtocolEncoderDecoder extends MessageToMessageCodec<BinaryWebSocke
         buf.writeShort(msg.command());
         buf.writeBytes(dataBytes);
 
-        out.add(new BinaryWebSocketFrame(buf));
-    }
-
-    @Override
-    protected void decode(ChannelHandlerContext ctx, BinaryWebSocketFrame msg, List<Object> out) {
-        ByteBuf in = msg.content();
-
-        int id = in.readInt();
-        short command = in.readShort();
-        String data = in.isReadable() ? in.toString(StandardCharsets.UTF_8) : null;
-        out.add(new StringMessage(id, command, data));
+        return new BinaryWebSocketFrame(buf);
     }
 
     @Override
